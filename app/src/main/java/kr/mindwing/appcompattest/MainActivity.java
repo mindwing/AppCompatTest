dc9c59d8215e745fd51ee0db1d5e874ef245c146
Jase HA <mindwing@gmail.com>
2016-01-30 14:49:58 +0900

    수정내용 반영

--- app/src/main/java/kr/mindwing/appcompattest/MainActivity.java
+++ app/src/main/java/kr/mindwing/appcompattest/MainActivity.java
@@ -1,7 +1,9 @@
 package kr.mindwing.appcompattest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        hideActionBar();
+    }
+
+    private void hideActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        String msg = actionBar == null ? "ActionBar is null" : "actionBar: " + actionBar.isHideOnContentScrollEnabled();
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
